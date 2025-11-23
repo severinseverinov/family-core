1955c14219c04c58e0a80be902aa6249dacbb080
Sevgin SERBEST (Severin Severinov ISAEV) <severinseverinov@hotmail.com>
2025-11-23 16:54:18 +0100

    nextconfig changed

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com", // Google giriş avatarları için
+      },
+      {
+        protocol: "https",
+        // BURAYI KENDİ SUPABASE URL'İNİN BAŞI İLE DEĞİŞTİR
+        // Örn: 'xyzproject.supabase.co'
+        hostname: "**.supabase.co",
+      },
+    ],
+  },
   /* config options here */
   reactCompiler: true,
 };
