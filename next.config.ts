f59ea2b528bdf6535166c7e210b9bcbff7b2e331
Sevgin SERBEST (Severin Severinov ISAEV) <severinseverinov@hotmail.com>
2025-11-23 16:55:21 +0100

    nextconfig changed-2

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
         protocol: "https",
         // BURAYI KENDİ SUPABASE URL'İNİN BAŞI İLE DEĞİŞTİR
         // Örn: 'xyzproject.supabase.co'
-        hostname: "**.supabase.co",
+        hostname: "gwbmjgvhbrzrrtsgecox.supabase.co",
       },
     ],
   },
